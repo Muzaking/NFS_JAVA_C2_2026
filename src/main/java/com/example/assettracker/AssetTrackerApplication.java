e5d3ca1993376e5c86b5e5df8e311b560f841bbe
Joaquin Antonio <jadiazumali@gmail.com>
2026-07-04 09:45:20 +0800

    Day 6 Spring Boot initial file structure

--- src/main/java/com/example/assettracker/AssetTrackerApplication.java
+++ src/main/java/com/example/assettracker/AssetTrackerApplication.java
@@ -1,5 +1,15 @@
 package com.example.assettracker;
-
+/**
+ * The main application class for the Asset Tracker.
+ * Normal run:
+ * mvn spring-boot:run
+ * 
+ * Run this local profile:
+ * mvn spring-boot:run -Dspring.profiles.active=local
+ * 
+ * For Windows PowerShell, use:
+ * mvn spring-boot:run "-Dspring-boot.run.profiles=local"
+ */
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
